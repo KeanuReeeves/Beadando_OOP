effb354bfc3176031f2c091db5013b3ccd56c1e0
tzas9 <tzas96@gmail.com>
2024-05-08 16:17:59 +0200

    second class

---
+++ main.py
@@ -0,0 +1,10 @@
+class Szoba:
+    def __init__(self, ar, szobaszam):
+        self.ar = ar
+        self.szobaszam = szobaszam
+
+class Egyagyasszoba(Szoba):
+    def __init__(self):
+        self.ar = 10
+        self.szobaszam = 1
+
